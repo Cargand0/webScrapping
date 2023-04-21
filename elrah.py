a1f649facf01f6c4fe0da49a80e7ee55689e8ada
Ahmad Fudhail <ahmadfudhail27@gmail.com>
2023-04-21 16:34:20 +0800

    Initial commit

---
+++ elrah.py
@@ -0,0 +1,43 @@
+#import library
+from urllib .request import Request, urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+#web url
+myUrl = 'https://elrahexclusive.my/product-category/baju-melayu/magnificent-4-0/?products-per-page=all'
+
+#request
+uClient = uReq(myUrl)
+page_html = uClient.read()
+uClient.close()
+
+#parse page
+page_soup = soup(page_html,"html.parser")
+
+#csv file
+fileName = "Elrah(Magnificent 4.0).csv"
+f = open(fileName, "w")
+
+headers = "Product Name,Product Price,Product Availability\n"
+f.write(headers)
+
+shoeContainers = page_soup.findAll("div",{"class":"product-inner clr"})
+
+for container in shoeContainers:
+    name_container = container.findAll("li",{"class":"title"})
+    prod_name = name_container[0].find("a").text
+    print("Product Name: " + prod_name)
+
+    price_container = container.findAll("span", {"class":"woocommerce-Price-amount amount"})
+    prod_price = price_container[0].find("bdi").text
+    print("Product Price: " + prod_price)
+
+    ava_container = container.findAll("li", {"class":"category"})
+    stock = ava_container[0].find("a").text
+    
+    print("Product Name: " + prod_name)
+    print("Product Price: " + prod_price)
+    print("image: " + stock)
+
+    f.write(prod_name.replace(",","|") + "," + prod_price.replace(",",".") + "," + stock + "\n")
+
+f.close()
